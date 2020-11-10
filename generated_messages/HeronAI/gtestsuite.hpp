2306f5e7a28c16083e0452add92695871521b668
Ken <kkroeger@araustech.com>
2020-11-09 20:24:39 -0500

    Added a Heron AI definition to the mavlink directory. It inherits Ardupilot as its base allowing us to expand upon the message set that we need.

---
+++ generated_messages/HeronAI/gtestsuite.hpp
@@ -0,0 +1,17 @@
+/** @file
+ *	@brief MAVLink comm testsuite protocol generated from HeronAI.xml
+ *	@see http://mavlink.org
+ */
+
+#pragma once
+
+#include <gtest/gtest.h>
+#include "HeronAI.hpp"
+
+#ifdef TEST_INTEROP
+using namespace mavlink;
+#undef MAVLINK_HELPER
+#include "mavlink.h"
+#endif
+
+
